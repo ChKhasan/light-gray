e5bf04e80283e95145dcd4c1ffca439c1e85873f
hasan.cholliyev <hasan.cholliyev@uzinfocom.uz>
2026-01-09 09:42:55 +0500

    Commented out user registration logic in `register.post.ts` for future revision.

--- server/api/auth/register.post.ts
+++ server/api/auth/register.post.ts
@@ -3,11 +3,11 @@ import { connectDB } from '../../utils/db'
 import { signToken } from '../../utils/jwt'
 
 export default defineEventHandler(async (event) => {
-    await connectDB()
-    const body = await readBody(event)
-
-    const user: any = await User.create(body)
-    const access_token = signToken({ id: user._id })
-
-    return { access_token }
+    // await connectDB()
+    // const body = await readBody(event)
+    //
+    // const user: any = await User.create(body)
+    // const access_token = signToken({ id: user._id })
+    //
+    // return { access_token }
 })
